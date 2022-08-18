9a24ad00b15d4d304a2af00ab0471bc2c3b5e4b6
M V K <krishna.mugala9@gmail.com>
2022-08-18 12:01:37 +0530

    added test cases and added java doc comments and formated code

--- src/test/java/com/jap/PhLevelAnalyserTest.java
+++ src/test/java/com/jap/PhLevelAnalyserTest.java
@@ -1,17 +1,53 @@
 package com.jap;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
-import static org.junit.Assert.*;
 
 public class PhLevelAnalyserTest {
+    PhLevelAnalyser input;
 
     @Before
-    public void setUp()  {
+    public void setUp() {
+        input = new PhLevelAnalyser();
     }
 
     @After
-    public void tearDown()  {
+    public void tearDown() {
+    }
+
+    @Test
+    // This is a test case for the PhLevelAnalyser class.
+    public void getPhValueWater() {
+        int phValue = 8;
+        String expectedResult = "pH value is fine";
+        //act
+        String actualresult = input.getPhValueOfWater(phValue);
+        //assertion
+        Assert.assertEquals(expectedResult, actualresult);
+    }
+
+    @Test
+    // This is a test case for the PhLevelAnalyser class.
+    public void phvalueforgreatertNum() {
+        int phValue = 10;
+        String expectedOutput = "pH value is high, partial water change required";
+        //act
+        String actualOutput = input.getPhValueOfWater(phValue);
+        //assertion
+        Assert.assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    // This is a test case for the PhLevelAnalyser class.
+    public void ph() { //arrange
+        int PhValue = 7;
+        String expectedOutput = "pH value is fine";
+        //act
+        String actualOutput = input.getPhValueOfWater(PhValue);
+        //ASSERTION
+        Assert.assertEquals(expectedOutput, actualOutput);
     }
 }
